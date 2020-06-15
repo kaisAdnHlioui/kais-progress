ad96195478a82bd6adcb4ca7b4a093d40db201ec
Kais Hlioui <kais.hlioui@adn-expertise.fr>
2020-06-15 12:17:49 +0100

    Updates

---
+++ precache-manifest.9f157ffa71303a6f9ca391ae514da501.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "302615ecbbb85162ad9381a3f691cec4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f6539dae26e664f6954d",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "841c44c663c62c0cb9f9",
+    "url": "./static/js/2.3b17b4e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.3b17b4e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6539dae26e664f6954d",
+    "url": "./static/js/main.eb115a42.chunk.js"
+  },
+  {
+    "revision": "dbbf59589e0370d56d97",
+    "url": "./static/js/runtime-main.0dc289a2.js"
+  }
+]);
